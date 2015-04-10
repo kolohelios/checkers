d3c69e13e430f7edd66c8b870aeb497ebcc162a5
Jon Edwards <jkedwards@me.com>
2015-04-09 20:50:49 -0700

    begin adding movement handling

--- client/index.js
+++ client/index.js
@@ -5,25 +5,48 @@ $(document).ready(init);
 function init(){
   paintCheckers();
   whichPlayerStartsFirst();
+  activePlayerToggle(false);
 }
 
 function paintCheckers() {
   for(var i = 0; i < 3; i++) {
     for(var j = (i === 1) ? 1 : 0; j < 8; j += 2){
       var $loc = $('[data-x=' + j + '][data-y=' + i + ']');
-      $loc.addClass('marmot-black');
+      $loc.addClass('p1').addClass('p1-pawn');
     }
   }
   for(var i = 5; i < 8; i++) {
-    for(var j = (i === 6) ? 1 : 0; j < 8; j += 2){
+    for(var j = (i === 6) ? 0 : 1; j < 8; j += 2){
       var $loc = $('[data-x=' + j + '][data-y=' + i + ']');
-      $loc.addClass('marmot-red');
+      $loc.addClass('p2').addClass('p2-pawn');
     }
   }
 }
 
 function whichPlayerStartsFirst(){
-  var randNumber = Math.floor(Math.random * 2);
-  console.log(randNumber);
-  $('#p' + randNumber).addClass('activeplayer');
+  var firstPlayer = Math.floor(Math.random() * 2) + 1;
+  $('#p' + firstPlayer).addClass('activeplayer');
+}
+
+function activePlayerToggle(togglePlayer) {
+  if(togglePlayer) {
+    $('.player').toggleClass('activeplayer');
+  }
+  if($('#p1').hasClass('activeplayer')) {
+    $('.p1').on('click', clickInSpace);
+    $('.p2').off('click');
+  }
+  else {
+    $('.p2').on('click', clickInSpace);
+    $('.p1').off('click');
+  }
+}
+
+function clickInSpace() {
+  if($('.hightlightedspace')) {
+    $(this).addClass('highlightedspace');
+  }
+  else {
+
+  }
 }
