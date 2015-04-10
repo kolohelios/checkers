1db87605265c4b82cc8fb4bf2963c0b7501827d8
Jon Edwards <jkedwards@me.com>
2015-04-09 19:59:14 -0700

    added checkersboard and icons

--- client/index.js
+++ client/index.js
@@ -1,2 +1,29 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+function init(){
+  paintCheckers();
+  whichPlayerStartsFirst();
+}
+
+function paintCheckers() {
+  for(var i = 0; i < 3; i++) {
+    for(var j = (i === 1) ? 1 : 0; j < 8; j += 2){
+      var $loc = $('[data-x=' + j + '][data-y=' + i + ']');
+      $loc.addClass('marmot-black');
+    }
+  }
+  for(var i = 5; i < 8; i++) {
+    for(var j = (i === 6) ? 1 : 0; j < 8; j += 2){
+      var $loc = $('[data-x=' + j + '][data-y=' + i + ']');
+      $loc.addClass('marmot-red');
+    }
+  }
+}
+
+function whichPlayerStartsFirst(){
+  var randNumber = Math.floor(Math.random * 2);
+  console.log(randNumber);
+  $('#p' + randNumber).addClass('activeplayer');
+}
