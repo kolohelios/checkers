a25696d91ff2592b566739a0acae010b4260f8f5
Jon Edwards <jkedwards@me.com>
2015-04-09 22:32:33 -0700

    ux framework getting close

--- client/index.js
+++ client/index.js
@@ -37,10 +37,10 @@ function activePlayerToggle(togglePlayer){
 
 function startSpaces(player){
   if($('#p1').hasClass('activeplayer')) {
-    setLegalSpaces([0,2],[2,2],[4,2],[6,2]);
+    setLegalSpaces([[0,2],[2,2],[4,2],[6,2]]);
   }
   else {
-    setLegalSpaces([1,6],[3,6],[5,6],[7,6]);
+    setLegalSpaces([[1,5],[3,5],[5,5],[7,5]]);
   }
 }
 
@@ -49,24 +49,39 @@ function clickToPick(){
     $(this).toggleClass('highlightedspace');
     var x = $(this).data('x');
     var y = $(this).data('y');
-    showLegalMoves(x, y);
-
+    findLegalMoves(x,y);
   }
   else {
     $(this).addClass('highlightedspace');
   }
 }
 
-function showLegalMoves(x, y){
+function findLegalMoves(x, y){
   //p1 pawn moves +y
   //p2 pawn moves -y
   var activePlayer = $('.activeplayer').attr('id');
   if(activePlayer === 'p1'){
-    //if()
+    y++;
     for(var i = 0; i < 3; i += 2){
-      console.log(x - i, y + 1);
+      isPositionOnBoard(i, y);
     }
   }
+  if(activePlayer === 'p2'){
+    y--;
+    for(var i = 0; i < 3; i += 2){
+      isPositionOnBoard(i, y);
+    }
+  }
+}
+function isPositionOnBoard(x, y){
+  var xIsGood = false, yIsGood = false;
+  if((x > -1) || (x < 8)){
+    xIsGood = true;
+  }
+  if((y > -1) || (y < 8)){
+    yIsGood = true;
+  }
+  return xIsGood && yIsGood;
 }
 
 function setLegalSpaces(arrayOfArrays){
@@ -74,6 +89,7 @@ function setLegalSpaces(arrayOfArrays){
   arrayOfArrays.forEach(function(array) {
     var x = array[0];
     var y = array[1];
+    console.log('x', x, 'y', y);
     var $loc = $('[data-x=' + x + '][data-y=' + y + ']');
     $loc.on('click', clickToPick);
   });
