3f6f9fe1056b9e9ab38bd6674df65b160619a20a
Jon Edwards <jkedwards@me.com>
2015-04-09 22:05:52 -0700

    more ux work

--- client/index.js
+++ client/index.js
@@ -6,9 +6,10 @@ function init(){
   paintCheckers();
   whichPlayerStartsFirst();
   activePlayerToggle(false);
+  startSpaces();
 }
 
-function paintCheckers() {
+function paintCheckers(){
   for(var i = 0; i < 3; i++) {
     for(var j = (i === 1) ? 1 : 0; j < 8; j += 2){
       var $loc = $('[data-x=' + j + '][data-y=' + i + ']');
@@ -28,25 +29,52 @@ function whichPlayerStartsFirst(){
   $('#p' + firstPlayer).addClass('activeplayer');
 }
 
-function activePlayerToggle(togglePlayer) {
+function activePlayerToggle(togglePlayer){
   if(togglePlayer) {
     $('.player').toggleClass('activeplayer');
   }
+}
+
+function startSpaces(player){
   if($('#p1').hasClass('activeplayer')) {
-    $('.p1').on('click', clickInSpace);
-    $('.p2').off('click');
+    setLegalSpaces([0,2],[2,2],[4,2],[6,2]);
   }
   else {
-    $('.p2').on('click', clickInSpace);
-    $('.p1').off('click');
+    setLegalSpaces([1,6],[3,6],[5,6],[7,6]);
   }
 }
 
-function clickInSpace() {
-  if($('.hightlightedspace')) {
-    $(this).addClass('highlightedspace');
-  }
-  else {
+function clickToPick(){
+  if($(this).hasClass('highlightedspace')){
+    $(this).toggleClass('highlightedspace');
+    var x = $(this).data('x');
+    var y = $(this).data('y');
+    showLegalMoves(x, y);
 
   }
+  else {
+    $(this).addClass('highlightedspace');
+  }
+}
+
+function showLegalMoves(x, y){
+  //p1 pawn moves +y
+  //p2 pawn moves -y
+  var activePlayer = $('.activeplayer').attr('id');
+  if(activePlayer === 'p1'){
+    //if()
+    for(var i = 0; i < 3; i += 2){
+      console.log(x - i, y + 1);
+    }
+  }
+}
+
+function setLegalSpaces(arrayOfArrays){
+  $('td').off('click');
+  arrayOfArrays.forEach(function(array) {
+    var x = array[0];
+    var y = array[1];
+    var $loc = $('[data-x=' + x + '][data-y=' + y + ']');
+    $loc.on('click', clickToPick);
+  });
 }
