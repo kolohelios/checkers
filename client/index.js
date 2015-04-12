7737ce9405c0e75e250f364da2681b794fae7fde
Jon Edwards <jkedwards@me.com>
2015-04-12 00:10:59 -0700

    bugs squashed

--- client/index.js
+++ client/index.js
@@ -2,6 +2,8 @@
 
 $(document).ready(init);
 
+var disableChangeHighlight = false;
+
 function init(){
   paintCheckers();
   whichPlayerStartsFirst();
@@ -33,25 +35,35 @@ function activePlayerToggle(){
 }
 
 function clickToPick(){
-  if($(this).hasClass('highlightedspace')){
-    $(this).removeClass('highlightedspace');
-    setSpacesForTurn();
+  if($('.highlightedspace').length > 0){
+    if($(this).hasClass('highlightedspace')){
+      if(disableChangeHighlight === false){
+        $(this).removeClass('highlightedspace');
+        setSpacesForTurn();
+      }
+    }
+    else{
+      if(movePiece(this) === 'jumpagain'){
+        disableChangeHighlight = true;
+      }
+      else{
+        activePlayerToggle();
+        setSpacesForTurn();
+        disableChangeHighlight = false;
+      }
+    }
   }
-  else if($('.highlightedspace').length === 0){
+  else{
     $(this).addClass('highlightedspace');
     var x = $(this).data('x');
     var y = $(this).data('y');
-    findLegalMoves(x, y);
-  }
-  else{
-    movePiece(this);
-    activePlayerToggle();
-    setSpacesForTurn();
+    findLegalMoves(x, y, true);
   }
   isThereAWinner();
+  console.log(disableChangeHighlight);
 }
 
-function findLegalMoves(x, y){
+function findLegalMoves(x, y, firstMove){
   clearClickableSpacesAndDebugHighlighting();
   setLegalSpace(x, y); // set current space
   var activePlayer = $('.activeplayer').attr('id');
@@ -63,21 +75,28 @@ function findLegalMoves(x, y){
   var y = loc.data('y');
   var yArray = (isKing) ? [1, -1] : [1];
   var yDirection = (activePlayer === 'p1') ? 1 : -1;
-  [1, -1].forEach(function(i) {
-    yArray.forEach(function(j) {
+  [1, -1].forEach(function(i){
+    yArray.forEach(function(j){
       if(isPositionOnBoard(x + i, y + (j * yDirection))){
-        if(isCompetitorAndPlayerNotInTheWay(x + i, y + (j * yDirection), activePlayer)){
-          setLegalSpace(x + i, y + (j * yDirection));
+        var nextPositionState = positionState(x + i, y + (j * yDirection), activePlayer);
+        if(nextPositionState === 'clear'){
+          if(firstMove === true){
+            setLegalSpace(x + i, y + (j * yDirection));
+          }
         }
-        else{
-          if(canJump(x + i * 2, y + (j * yDirection * 2), activePlayer, isKing)){
+        else if(nextPositionState === 'competitor'){
+          var jumpPositionState = positionState(x + i * 2, y + (j * yDirection * 2), activePlayer);
+          if(jumpPositionState === 'clear'){
+            console.log('jumpPositionState is clear');
             setLegalSpace(x + i * 2, y + (j * yDirection * 2));
           }
         }
       }
     });
   });
-  console.log($('.debughighlight').length - 1);
+  var legalMovesAvailable = $('.debughighlight').length - 1;
+  console.log('legal moves available', legalMovesAvailable);
+  return legalMovesAvailable;
 }
 
 function setSpacesForTurn(){
@@ -108,37 +127,32 @@ function isPositionOnBoard(x, y){
   return xIsGood && yIsGood;
 }
 
-function isCompetitorAndPlayerNotInTheWay(x, y, player){
+function positionState(x, y, player){
   var competitor = (player === 'p1') ? 'p2' : 'p1';
   var loc = $('[data-x=' + x + '][data-y=' + y + ']');
-  if(($(loc).hasClass(player)) || ($(loc).hasClass(competitor))){
+  if($(loc).hasClass(player)){
+    return 'player';
+  }
+  else if($(loc).hasClass(competitor)){
+    return 'competitor';
+  }
+  else{
+    return 'clear';
+  }
+}
+
+/*
+function canJump(x, y, player){
+  var loc = $('[data-x=' + x + '][data-y=' + y + ']');
+  var competitor = (player === 'p1') ? 'p2' : 'p2';
+  if($(loc).hasClass(competitor)){
     return false;
   }
   else{
     return true;
   }
 }
-
-function canJump(x, y, player, isKing){
-  var loc = $('[data-x=' + x + '][data-y=' + y + ']');
-  if(isKing){
-    if(($(loc).hasClass('p1')) || ($(loc).hasClass('p2'))){
-      return false;
-    }
-    else{
-      return true;
-    }
-  }
-  else{
-    var competitor = (player === 'p1') ? 'p2' : 'p2';
-    if($(loc).hasClass(competitor)){
-      return false;
-    }
-    else{
-      return true;
-    }
-  }
-}
+*/
 
 function setLegalSpace(x, y){
     var $loc = $('[data-x=' + x + '][data-y=' + y + ']');
@@ -150,18 +164,26 @@ function movePiece(space){
   var activePlayer = $('.activeplayer').attr('id');
   var $originSpace = $('.highlightedspace');
   var competitor = (activePlayer === 'p1') ? 'p2' : 'p1';
-  var subsequentJump = false;
+  var subsequentJumpAvailable = false;
   if (Math.abs(($(space).data('x') - $originSpace.data('x'))) === 2){
     removeCompetitorPiece(space, $originSpace, competitor);
-    var subsequentJump = findLegalMoves();
+    var pieceRemoved = true;
   }
   var originClasses = $originSpace.attr('class'); //.removeClass(activePlayer).removeClass(activePlayer + '-pawn').removeClass('highlightedspace');
   var spaceClasses = $(space).attr('class');
   $(space).attr('class', originClasses);
   $originSpace.attr('class', spaceClasses);
+  if(pieceRemoved){
+    $(space).addClass('highlightedspace');
+    subsequentJumpAvailable = findLegalMoves($(space).data('x'), $(space).data('y'), false) > 0 ? true : false;
+    console.log('subsequent jump available', subsequentJumpAvailable);
+  }
   checkForCrowning(activePlayer, space);
-  if(subsequentJump){
-
+  if(subsequentJumpAvailable){
+    return 'jumpagain';
+  }
+  else{
+    return 'endofturn';
   }
 }
 
@@ -182,10 +204,6 @@ function removeCompetitorPiece(space, $originSpace, competitor){
   }
   var $middleSpace = $('[data-x=' + middleX + '][data-y=' + middleY + ']');
   $middleSpace.removeClass(competitor + ' ' + competitor + '-pawn' + ' ' + competitor + '-king');
-}
-
-function checkForSubsequentJump(){
-  return false;
 }
 
 function createArrayOfPlayerSpaces(player) {
